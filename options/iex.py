c2e96a97729cd151f9b49876ef7c20bd1eb77836
Gustavo A. Oliver <gstvolvr@users.noreply.github.com>
2020-07-19 07:54:27 -0400

    options calculator

--- options/iex.py
+++ options/iex.py
@@ -20,11 +20,21 @@ class IEX:
 
     def get_industry(self, ticker):
         data = self.__get(f'{self.base_url}/stable/stock/{ticker}/company?token={self.token}')
-        return data['industry']
+        if data:
+            return data['industry']
 
-    def get_price(self, ticker):
-        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/price?&token={self.token}')
+    def get_price(self, ticker, date=None):
+        if date is None:
+            data = self.__get(f'{self.base_url}/stable/stock/{ticker}/price?token={self.token}')
+        else:
+            price_list = self.__get(f'{self.base_url}/stable/stock/{ticker}/chart/1d/{date}?token={self.token}')
+            data = sorted(price_list, key=lambda d: d['minute'])[-1]['marketClose']
         return data
+
+    def get_quote(self, ticker):
+        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/quote?token={self.token}')
+        if data:
+            return data
 
     def has_options(self, ticker):
         dates = self.get_call_expiration_dates(ticker=ticker)
@@ -34,10 +44,16 @@ class IEX:
         return False
 
     def get_last_dividend(self, ticker, time_frame='ytd'):
-        return self.__get(f'{self.base_url}/stable/stock/{ticker}/dividends/{time_frame}?token={self.token}')
+        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/dividends/{time_frame}?token={self.token}')
+        if data:
+            return sorted(data, key=lambda d: d['exDate'])[-1]
+        return {}
 
     def get_next_dividend(self, ticker):
-        return self.__get(f'{self.base_url}/stable/stock/{ticker}/dividends/next?token={self.token}')
+        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/dividends/next?token={self.token}')
+        if data:
+            return data
+        return {}
 
     def get_call_expiration_dates(self, ticker):
         return self.__get(f'{self.base_url}/stable/stock/{ticker}/options?token={self.token}')
@@ -50,3 +66,8 @@ class IEX:
 
     def get_all_options_dates(self):
         return self.__get(f'{self.base_url}/ref-data/options/symbols?token={self.token}')
+
+    def get_earnings_per_share(self, ticker):
+        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/earnings/1/actualEPS?token={self.token}')
+        if data:
+            return data
