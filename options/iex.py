3f42313da1e575b0f9069d546dbf2fd31d38c7fc
Gustavo A. Oliver <gustavo.oliver@protonmail.com>
2020-08-17 21:54:13 -0400

    update iex object

--- options/iex.py
+++ options/iex.py
@@ -1,73 +1,97 @@
 import requests
+import logging
+
+
+def _get(url, json=True):
+    try:
+        response = requests.get(url)
+        data = response.json() if json else response.text
+    except Exception as e:
+        logging.debug(f'ERROR: {response.text}')
+        if json:
+            data = {}
+        else:
+            data = None
+    return data
+
 
 class IEX:
     def __init__(self):
         self.token = None
-        self.base_url = None
+        self.base_url = 'https://cloud.iexapis.com'
+        self.stats = {}
 
-    def __get(self, url, json=True):
-        try:
-            response = requests.get(url)
-            data = response.json() if json else response.text
+    def get_company(self, symbol):
+        data = _get(f'{self.base_url}/stable/stock/{symbol}/company?token={self.token}')
+        return data or {}
 
-        except:
-            return None
-        return data
-
-    def get_quote(self, ticker):
-        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/batch?types=quote&token={self.token}')
+    def get_quote(self, symbol):
+        data = _get(f'{self.base_url}/stable/stock/{symbol}/batch?types=quote&token={self.token}')
         return data['quote']
 
-    def get_industry(self, ticker):
-        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/company?token={self.token}')
+    # TODO: remove method
+    def get_industry(self, symbol):
+        data = _get(f'{self.base_url}/stable/stock/{symbol}/company?token={self.token}')
         if data:
             return data['industry']
 
-    def get_price(self, ticker, date=None):
+    def get_price(self, symbol, date=None):
         if date is None:
-            data = self.__get(f'{self.base_url}/stable/stock/{ticker}/price?token={self.token}')
+            data = _get(f'{self.base_url}/stable/stock/{symbol}/price?token={self.token}')
         else:
-            price_list = self.__get(f'{self.base_url}/stable/stock/{ticker}/chart/1d/{date}?token={self.token}')
+            price_list = _get(f'{self.base_url}/stable/stock/{symbol}/chart/1d/{date}?token={self.token}')
             data = sorted(price_list, key=lambda d: d['minute'])[-1]['marketClose']
         return data
 
-    def get_quote(self, ticker):
-        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/quote?token={self.token}')
+    def get_quote(self, symbol):
+        data = _get(f'{self.base_url}/stable/stock/{symbol}/quote?token={self.token}')
         if data:
             return data
 
-    def has_options(self, ticker):
-        dates = self.get_call_expiration_dates(ticker=ticker)
+    def get_stats(self, symbol):
+        if symbol in self.stats:
+            return self.stats[symbol]
+        else:
+            self.stats = {symbol: _get(f'{self.base_url}/stable/stock/{symbol}/stats?token={self.token}') or {}}
+        return self.stats[symbol]
 
-        if dates:
-            return len(dates) > 0
-        return False
+    def get_dividend_yield(self, symbol):
+        symbol_stats = self.get_stats(symbol)
+        if 'dividendYield' in symbol_stats:
+            return symbol_stats['dividendYield']
+        return None
 
-    def get_last_dividend(self, ticker, time_frame='ytd'):
-        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/dividends/{time_frame}?token={self.token}')
+    def has_options(self, symbol):
+        dates = self.get_call_expiration_dates(symbol=symbol)
+        return dates is not None and len(dates) > 0
+
+    def has_dividends(self, symbol):
+        symbol_stats = self.get_stats(symbol)
+        return ('nextDividendDate' in symbol_stats and symbol_stats['nextDividendDate'] is not None) or \
+               ('exDividendDate' in symbol_stats and symbol_stats['exDividendDate'] is not None)
+
+    def get_last_dividend(self, symbol, time_frame='1y'):
+        data = _get(f'{self.base_url}/stable/stock/{symbol}/dividends/{time_frame}?token={self.token}')
         if data:
             return sorted(data, key=lambda d: d['exDate'])[-1]
         return {}
 
-    def get_next_dividend(self, ticker):
-        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/dividends/next?token={self.token}')
-        if data:
-            return data
-        return {}
+    def get_next_dividend(self, symbol):
+        return _get(f'{self.base_url}/stable/stock/{symbol}/dividends/next?token={self.token}') or {}
 
-    def get_call_expiration_dates(self, ticker):
-        return self.__get(f'{self.base_url}/stable/stock/{ticker}/options?token={self.token}')
+    def get_call_expiration_dates(self, symbol):
+        return _get(f'{self.base_url}/stable/stock/{symbol}/options?token={self.token}')
 
-    def get_calls(self, ticker, expiration_date):
-        return self.__get(f'{self.base_url}/stable/stock/{ticker}/options/{expiration_date}/call?token={self.token}')
+    def get_calls(self, symbol, expiration_date):
+        return _get(f'{self.base_url}/stable/stock/{symbol}/options/{expiration_date}/call?token={self.token}')
 
-    def get_symbols_csv(self):
-        return self.__get(f'{self.base_url}/beta/ref-data/symbols?token={self.token}&format=csv', json=False)
+    def get_all_symbols(self):
+        return _get(f'{self.base_url}/beta/ref-data/symbols?token={self.token}')
 
     def get_all_options_dates(self):
-        return self.__get(f'{self.base_url}/ref-data/options/symbols?token={self.token}')
+        return _get(f'{self.base_url}/ref-data/options/symbols?token={self.token}')
 
-    def get_earnings_per_share(self, ticker):
-        data = self.__get(f'{self.base_url}/stable/stock/{ticker}/earnings/1/actualEPS?token={self.token}')
+    def get_earnings_per_share(self, symbol):
+        data = _get(f'{self.base_url}/stable/stock/{symbol}/earnings/1/actualEPS?token={self.token}')
         if data:
             return data
