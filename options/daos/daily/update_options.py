c2f05676274d0d77caadb91df7d8462d0bfdd14d
Gustavo A. Oliver <gustavo.oliver@protonmail.com>
2020-08-19 23:27:28 -0400

    parallelize options

--- options/daos/daily/update_options.py
+++ options/daos/daily/update_options.py
@@ -1,10 +1,10 @@
-import logging
-import psycopg2
-import options.iex
-import psycopg2.extras
 import datetime
-import time
+import logging
+import multiprocessing
+import options.iex
 import os
+import psycopg2
+import psycopg2.extras
 from options import util
 
 root = logging.getLogger()
@@ -14,7 +14,7 @@ root.setLevel(logging.INFO)
 def update_eod_options(conn, iex):
 
     sql = """
-    SELECT d.symbol, d.ex_date 
+    SELECT d.symbol, d.ex_date
     FROM universe.dividends d
     INNER JOIN universe.eod_prices p
     ON d.symbol = p.symbol
@@ -24,7 +24,7 @@ def update_eod_options(conn, iex):
         GROUP BY symbol) o
     ON d.symbol = o.symbol
     WHERE
-        (d.gross_annual_yield / p.previous_stock_price) > 0.01 AND
+        (d.gross_annual_yield / p.previous_stock_price) > 0.02 AND
         p.previous_stock_price > 7.5 AND
         d.ex_date > NOW() AND
         (o.latest_update IS NULL OR o.latest_update < (NOW() - INTERVAL '12 hours'));
@@ -35,56 +35,61 @@ def update_eod_options(conn, iex):
         symbol, id, expiration_date, contract_size, strike_price, closing_price, last_updated,
         type, volume, bid, ask, is_adjusted
     ) VALUES (
-        %(symbol)s, 
-        %(id)s, 
-        %(expiration_date)s, 
-        %(contract_size)s, 
-        %(strike_price)s, 
-        %(closing_price)s, 
+        %(symbol)s,
+        %(id)s,
+        %(expiration_date)s,
+        %(contract_size)s,
+        %(strike_price)s,
+        %(closing_price)s,
         %(last_updated)s,
-        %(type)s, 
-        %(volume)s, 
-        %(bid)s, 
-        %(ask)s, 
+        %(type)s,
+        %(volume)s,
+        %(bid)s,
+        %(ask)s,
         %(is_adjusted)s
     )
     ON CONFLICT(id)
     DO UPDATE SET
-        symbol = EXCLUDED.symbol, 
-        id = EXCLUDED.id, 
-        expiration_date = EXCLUDED.expiration_date, 
-        contract_size = EXCLUDED.contract_size, 
-        strike_price = EXCLUDED.strike_price, 
-        closing_price = EXCLUDED.closing_price, 
+        symbol = EXCLUDED.symbol,
+        id = EXCLUDED.id,
+        expiration_date = EXCLUDED.expiration_date,
+        contract_size = EXCLUDED.contract_size,
+        strike_price = EXCLUDED.strike_price,
+        closing_price = EXCLUDED.closing_price,
         last_updated = EXCLUDED.last_updated,
-        type = EXCLUDED.type, 
-        volume = EXCLUDED.volume, 
-        bid = EXCLUDED.bid, 
-        ask = EXCLUDED.ask, 
+        type = EXCLUDED.type,
+        volume = EXCLUDED.volume,
+        bid = EXCLUDED.bid,
+        ask = EXCLUDED.ask,
         is_adjusted = EXCLUDED.is_adjusted
     """
-
-    start = time.time()
 
     with conn.cursor() as update_cursor:
         with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
             cursor.execute('TRUNCATE universe.eod_call_options CASCADE;')
             cursor.execute(sql)
 
-            for i, (row) in enumerate(cursor.fetchall()):
-                symbol, min_contract_date = row['symbol'], datetime.datetime.strftime(row['ex_date'], '%Y%m')
-                if (i != 0) and (i % 50) == 0:
-                    logging.info(f'processed: {i} in {(time.time() - start) / 60.:.2f} minutes')
+            rows = [row for row in cursor.fetchall()]
 
-                dates = iex.get_call_expiration_dates(symbol)
+            with multiprocessing.Pool(4) as p:
+                params = p.map(_process, rows)
+            flattened_params = [p for symbol_params in params for p in symbol_params]
+            update_cursor.execute(update_sql, flattened_params)
 
-                for expiration_date in dates:
-                    # TODO: remove 2021 requirement
-                    if expiration_date >= min_contract_date and expiration_date[:4] == '2021':
-                        results = iex.get_calls(symbol, expiration_date)
-                        if results:
-                            params = [{util.to_snake(k): v for k, v in instance.items()} for instance in results]
-                            update_cursor.executemany(update_sql, params)
+
+def _process(row):
+    symbol, min_contract_date = row['symbol'], datetime.datetime.strftime(row['ex_date'], '%Y%m')
+
+    dates = iex.get_call_expiration_dates(symbol)
+    params = []
+
+    for expiration_date in dates:
+        # TODO: remove 2021 requirement
+        if expiration_date >= min_contract_date and expiration_date[:4] == '2021':
+            results = iex.get_calls(symbol, expiration_date)
+            if results:
+                params.extend([{util.to_snake(k): v for k, v in instance.items()} for instance in results])
+    return params
 
 
 if __name__ == '__main__':
