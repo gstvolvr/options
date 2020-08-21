facfdff86f54378c5a53f5d1c3a334dfa72fd41a
root <root@scheduler.us-east4-c.c.options-282500.internal>
2020-08-21 12:38:10 +0000

    update dividends

--- bin/daily.sh
+++ bin/daily.sh
@@ -11,6 +11,8 @@ source $APP_DIR/venv/bin/activate
 
 time PYTHONPATH=$APP_DIR python3 $APP_DIR/options/daos/daily/update_prices.py 
 sleep 5
+time PYTHONPATH=$APP_DIR python3 $APP_DIR/options/daos/daily/update_dividends.py 
+sleep 5
 time PYTHONPATH=$APP_DIR python3 $APP_DIR/options/daos/daily/update_options.py 
 sleep 5
 time PYTHONPATH=$APP_DIR python3 $APP_DIR/options/daos/daily/update_returns.py 
