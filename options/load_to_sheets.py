8c38a1c75b87511d120b0a44c1ea1b76952b2c3f
Gustavo A. Oliver <gstvolvr@users.noreply.github.com>
2022-07-07 19:34:41 -0400

    initialize using psycopg2

--- options/load_to_sheets.py
+++ options/load_to_sheets.py
@@ -1,21 +1,62 @@
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-import os.path
-import csv
 import collections
+import csv
 import gc
-import time
+import os.path
+import psycopg2
+import psycopg2.extras
 import socket
-import logging
+import time
 
 socket.setdefaulttimeout(300)
 
 BATCH_SIZE = 250
 MAX_RETRIES = 3
-SPREADSHEET_ID = os.getenv('GOOGLE_SHEETS_ID')
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SECRET_PATH = os.getenv('GOOGLE_SHEETS_CLIENT_SECRET')
 SLEEP_SECONDS = 0.75
+
+
+UPDATE_TABLE = """
+    INSERT INTO universe.returns(
+        symbol,
+        company_name,
+        industry,
+        last,
+        net,
+        strike_price,
+        expiration_date,
+        insurance,
+        premium,
+        dividend_amount,
+        dividend_ex_date,
+        return_after_1_div,
+        return_after_2_div,
+        return_after_3_div,
+        return_after_4_div,
+        bid,
+        mid,
+        ask,
+        quote_date
+    ) VALUES (
+        %(symbol)s,
+        %(company_name)s,
+        %(industry)s,
+        %(last)s,
+        %(net)s,
+        %(strike_price)s,
+        %(expiration_date)s,
+        %(insurance)s,
+        %(premium)s,
+        %(dividend_amount)s,
+        %(dividend_ex_date)s,
+        %(return_after_1_div)s,
+        %(return_after_2_div)s,
+        %(return_after_3_div)s,
+        %(return_after_4_div)s,
+        %(bid)s,
+        %(mid)s,
+        %(ask)s,
+        %(quote_date)s
+    )
+"""
 
 
 def main(data_path):
@@ -46,76 +87,39 @@ def main(data_path):
 
     values = []
 
-    creds = service_account.Credentials.from_service_account_file(SECRET_PATH, scopes=SCOPES)
-    SHEET_NAME = 'data'
-    RANGE_NAME= f'{SHEET_NAME}!A{{row_number}}'
+    with psycopg2.connect(database='postgres', user='postgres', host='10.60.176.3', port=5432) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            with open(f'{data_path}/returns.csv', 'r') as f:
+                returns = csv.DictReader(f)
 
-    service = build('sheets', 'v4', credentials=creds, cache_discovery=False)
-    # clear current data in the spreadsheet
-    service.spreadsheets().values().clear(spreadsheetId=SPREADSHEET_ID, range=SHEET_NAME).execute()
-    body = {'values': [list(cols.keys())]}
-    service.spreadsheets().values().update(
-        spreadsheetId=SPREADSHEET_ID,
-        range=RANGE_NAME.format(row_number=1),
-        valueInputOption='RAW',
-        body=body).execute()
+                rows = []
+                for i, row in enumerate(returns):
+                    result = {}
+                    ordered_result = []
+                    for col, type_func in cols.items():
+                        if col in ['company_name', 'industry']:
+                            if row['symbol'] in companies:
+                                result[col] = companies[row['symbol']][col]
+                            else:
+                                result[col] = None
+                        else:
+                            result[col] = type_func(row[col]) if row[col] else ''
+                    values.append(ordered_result)
 
-    row_number = 2
-    with open(f'{data_path}/returns.csv', 'r') as f:
-        returns = csv.DictReader(f)
-
-        for i, row in enumerate(returns):
-            ordered_result = []
-            for col, type_func in cols.items():
-                if col in ['company_name', 'industry']:
-                    if row['symbol'] in companies:
-                        ordered_result.append(companies[row['symbol']][col])
-                    else:
-                        ordered_result.append(None)
-                else:
-                    value = type_func(row[col]) if row[col] else ''
-                    ordered_result.append(value)
-            values.append(ordered_result)
-
-            if i % BATCH_SIZE == 0 and i != 0:
-                # order by: symbol, expiration_date, strike_price
+                    if i % BATCH_SIZE == 0 and i != 0:
+                        # order by: symbol, expiration_date, strike_price
+                        values = sorted(values, key=lambda r: (r[0],
+                                                               r[6],
+                                                               r[5]))
+                        cursor.executemany(UPDATE_TABLE, values)
+                        values = []
+                        gc.collect()
+                        # see usage limits: https://developers.google.com/sheets/api/limits
                 values = sorted(values, key=lambda r: (r[0],
                                                        r[6],
                                                        r[5]))
-                body = {'values': list(map(list, values))}
-                try_n, try_again = 0, True
-                
-                while try_again:
-                    try:
-                        service.spreadsheets().values().update(
-                            spreadsheetId=SPREADSHEET_ID,
-                            range=RANGE_NAME.format(row_number=row_number),
-                            valueInputOption='RAW',
-                            body=body).execute()
-                        try_again = False
-                    except socket.timeout:
-                        logging.warn(f'socket timeout....retry # {try_n}')
-                        try_n += 1
-                        try_again = try_n < MAX_RETRIES 
 
-                        if try_n == MAX_RETRIES:
-                            raise Exception('Reached max retries')
-
-
-                values = []
-                gc.collect()
-                # see usage limits: https://developers.google.com/sheets/api/limits
-                row_number += BATCH_SIZE
-                time.sleep(SLEEP_SECONDS)
-        values = sorted(values, key=lambda r: (r[0],
-                                               r[6],
-                                               r[5]))
-        body = {'values': list(map(list, values))}
-        service.spreadsheets().values().update(
-            spreadsheetId=SPREADSHEET_ID,
-            range=RANGE_NAME.format(row_number=row_number),
-            valueInputOption='RAW',
-            body=body).execute()
+                cursor.executemany(UPDATE_TABLE, values)
 
 if __name__ == '__main__':
     main(data_path=os.getenv('DATA_PATH'))
