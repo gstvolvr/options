8cd95d22e82e1f9070a35a4e41e57227ff3ec0d2
Gustavo Oliver <gstvolvr@users.noreply.github.com>
2022-07-06 21:50:20 -0400

    set up td ameritrade api (#11)

--- bin/daily.sh
+++ bin/daily.sh
@@ -6,11 +6,12 @@ SCRIPT=`readlink -f $0`
 SCRIPT_DIR=`dirname $SCRIPT`
 APP_DIR=$SCRIPT_DIR/..
 
+echo $APP_DIR
 export $(cat $HOME/.dev/.env | xargs)
-source $APP_DIR/venv/bin/activate
+source $APP_DIR/.venv/bin/activate
 
-time PYTHONPATH=$APP_DIR python3 $APP_DIR/options/daos/daily/update_options.py 
-sleep 5
-time PYTHONPATH=$APP_DIR python3 $APP_DIR/options/daos/daily/update_returns.py 
-sleep 5
+time PYTHONPATH=$APP_DIR python3 $APP_DIR/options/daos/daily/update_chains.py
+sleep 1
+time PYTHONPATH=$APP_DIR python3 $APP_DIR/options/daos/daily/update_returns.py
+sleep 1
 time PYTHONPATH=$APP_DIR python3 $APP_DIR/options/load_to_sheets.py
---
+++ options/td_ameritrade.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import logging
+import requests
+
+REQUEST_DATE_FORMAT = '%Y%m%d'
+
+def _get(url, json=True):
+    try:
+        response = requests.get(url)
+        data = response.json() if json else response.text
+    except Exception as e:
+        logging.debug(f'ERROR: {e}')
+        if json:
+            data = {}
+        else:
+            data = None
+    return data
+
+
+def _convert_str_format(date, current_date_format):
+    return datetime.strptime(date, current_date_format).strftime(REQUEST_DATE_FORMAT)
+
+class TDAmeritrade:
+    def __init__(self):
+        self.token = None
+        self.base_url = 'https://api.tdameritrade.com/v1'
+
+    def get_chains(self, ticker: str, from_date: str):
+        return _get(f'{self.base_url}/marketdata/chains?apikey={self.token}&symbol={ticker}&contractType=CALL&includeQuotes=TRUE&optionType=S&fromDate={from_date}')
