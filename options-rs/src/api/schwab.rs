6b2f0d67d2cbb0082bb262b6db81cbc26e257b8a
Gustavo A. Oliver <g.antonio.oliver@gmail.com>
2025-05-13 22:07:15 -0400

    Properly get quote output

--- options-rs/src/api/schwab.rs
+++ options-rs/src/api/schwab.rs
@@ -1,42 +1,38 @@
 use std::error::Error;
-use reqwest::header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE};
-use serde::de::DeserializeOwned;
 use crate::api::quote::QuoteApiResponse;
 use crate::api::chains::ChainsApiResponse;
-use crate::api::auth::MARKET_DATA_API_URL;
+use crate::api::auth::{OAuthClient, MARKET_DATA_API_URL};
 
-
-pub async fn call_api<T: DeserializeOwned>(api_url: &str) -> Result<T, Box<dyn Error>> {
-    let client = reqwest::Client::new();
-    let mut headers = HeaderMap::new();
-
-    headers.insert(AUTHORIZATION, format!("Bearer {}", "your_token_here").parse()?);
-    headers.insert(CONTENT_TYPE, "application/json".parse()?);
-
-    let response = client
-        .get(api_url)
-        .headers(headers)
-        .send()
-        .await?;
+pub async fn quote(symbol: &str, oauth_client: &OAuthClient) -> Result<QuoteApiResponse, Box<dyn Error>> {
+    let api_url = format!("{}/quotes?symbols={}&fields=quote,reference&indicative=false", MARKET_DATA_API_URL, symbol);
+    println!("{}", &api_url);
+    let response = oauth_client.get(&api_url).await?;
+    println!("{:?}", response);
 
     if response.status().is_success() {
-        let body = response.text().await?;
-        let api_response: T = serde_json::from_str(&body)?;
-        Ok(api_response)
+        let text = response.text().await?;
+        let json: serde_json::Map<String, serde_json::Value> = serde_json::from_str(&text)?;
+
+        // Take first entry since we only request one symbol
+        if let Some((_symbol, quote_data)) = json.into_iter().next() {
+            println!("symbol: {:?}", _symbol);
+            println!("quote: {:?}", quote_data);
+            let api_response: QuoteApiResponse = serde_json::from_value(quote_data)?;
+            Ok(api_response)
+        } else {
+            Err("No quote data found in response".into())
+        }
     } else {
-        Err(format!("Request failed with status: {}", response.status()).into())
+        let status = response.status();
+        let error_text = response.text().await?;
+        println!("Error response body: {}", error_text);
+        Err(format!("Request failed with status: {}", status).into())
     }
 }
 
-
-pub async fn quote(symbol: &str) -> Result<QuoteApiResponse, Box<dyn Error>> {
-    let api_url = format!("{}/quotes?symbol={}", MARKET_DATA_API_URL, symbol);
-    let response = call_api::<QuoteApiResponse>(&api_url).await?;
-    return Ok(response);
-}
-
+// TODO: fill in
 pub async fn chains(symbol: &str) -> Result<ChainsApiResponse, Box<dyn Error>> {
     let api_url = format!("{}/chains?symbols={}&fields=quote&indicative=false", MARKET_DATA_API_URL, symbol);
     let response: ChainsApiResponse = call_api(&api_url).await?;
-    return Ok(response);
+    Ok(response)
 }
