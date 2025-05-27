16de931b9691455caf04c67ced567567c6ddc4a5
Gustavo A. Oliver <g.antonio.oliver@gmail.com>
2025-05-27 07:44:00 -0400

    clarify return calculations

--- options-rs/src/utils.rs
+++ options-rs/src/utils.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, NaiveDate};
+use chrono::{Datelike, DateTime, Duration, NaiveDate, Utc, Months};
 use csv::ReaderBuilder;
 use phf::phf_map;
 use serde::Deserialize;
@@ -103,7 +103,7 @@ pub fn parse_date(date_str: &str) -> Result<NaiveDate, Box<dyn Error>> {
     if date_str.contains('T') {
         // Parse ISO 8601 format (with time component)
         match DateTime::parse_from_rfc3339(date_str) {
-            Ok(datetime) => Ok(datetime.date_naive()),
+            Ok(datetime) => Ok(datetime.with_timezone(&Utc).date_naive()),
             Err(e) => Err(Box::new(e))
         }
     } else {
@@ -118,6 +118,29 @@ pub fn parse_date(date_str: &str) -> Result<NaiveDate, Box<dyn Error>> {
 pub fn round_to_decimals(value: f64, decimals: Option<f64>) -> f64 {
     let decimals = decimals.unwrap_or(2.0);
     (value * 10.0_f64.powf(decimals)).round() / (10.0_f64.powf(decimals))
+}
+
+/// If a date falls on a weekend, pick the prior Friday
+pub fn get_previous_weekday(date: NaiveDate) -> NaiveDate {
+    let mut date = date;
+    let weekday = date.weekday().num_days_from_monday();
+    if weekday == 6 { // Sunday
+        date = date - Duration::days(2)
+    } else if weekday == 5 { // Saturday
+        date = date - Duration::days(1)
+    }
+    date
+}
+
+/// Projects a given date forward by a quarter (3 months) or whatever the expected dividend frequency is.
+/// Then adjusts it to the previous Friday if the projected date falls on a Saturday or Sunday.
+/// This accounts for a common financial calendar rule for ex-dividend dates.
+///
+/// TODO: look at historical patterns / ticker to determine most likely dividend date
+pub fn estimate_next_dividend_date(date: NaiveDate, n_dividends: i64, div_freq: i64) -> NaiveDate {
+    let months_between_dividends = 12 / div_freq;
+    let months_until_next_dividend_capture = months_between_dividends * n_dividends;
+    date.checked_add_months(Months::new(months_until_next_dividend_capture as u32)).unwrap()
 }
 
 #[cfg(test)]
